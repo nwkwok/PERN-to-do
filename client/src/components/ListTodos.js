4d7a46f60d741ed11eed8e92381ec2fc7f1cc6be
nwkwok <nwkwok@gmail.com>
2020-10-13 23:55:09 -0700

    connect DELETE button to API

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -3,6 +3,20 @@ import React, {Fragment, useEffect, useState} from 'react';
 const ListTodos = () => {
 
     const [todos, setTodos] = useState([]);
+
+    //DELETE Todos
+    const deleteTodo = async id => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            });
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+
     const getTodos = async() => {
         try {
             const response = await fetch("http://localhost:5000/todos")
@@ -39,7 +53,14 @@ const ListTodos = () => {
                     <tr key={todo.todo_id}>
                         <td>{todo.description}</td>
                         <td>Edit</td>
-                        <td><button className="btn btn-danger">Delete</button></td>
+                        <td>
+                            <button 
+                                className="btn btn-danger" 
+                                onClick={() => deleteTodo(todo.todo_id)}
+                                >
+                                Delete
+                                </button>
+                        </td>
                     </tr>
                 ))}
 

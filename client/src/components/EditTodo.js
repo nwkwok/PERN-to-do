8b6142e3dfc6a0d2c6e9f1972165cc927b35412a
nwkwok <nwkwok@gmail.com>
2020-10-14 23:04:46 -0700

    add modal to edit component

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 
-const EditTodo = () => {
+const EditTodo = (props) => {
+    console.log(props.todo);
     return(
-        <h1>Edit Todo</h1>
+        <Fragment>
+<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal">
+ Edit
+</button>
+
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <h4 class="modal-title">Edit</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <div class="modal-body">
+        <input className='form-control' type='text' />
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-warning" data-dismiss="modal">Edit</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+        </Fragment>
+        
     )
 }
 

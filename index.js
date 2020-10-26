7abdece40f9a7fc9091a7cf633595265a12e005a
nwkwok <nwkwok@gmail.com>
2020-10-26 14:24:09 -0700

    fix middleware catchall error

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.delete('/todos/:id', async(req, res) => {
 })
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/builder/index.html"))
+    res.sendFile(path.join(__dirname, "client/build/index.html"))
 })
 
 
